60a4e8979cb756ca77b62045b6f62304b7f73840
Neu <neusradioxxx@gmail.com>
2023-04-03 12:33:49 +0330

    Update cronjob script with correct priviliges and colors

--- scripts/xt_geoip_update.sh
+++ scripts/xt_geoip_update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Colors
+RESET="\033[0m"
+RED="\033[31m"
+GREEN="\033[32m"
+B_RED="\033[1;31m"
+B_GREEN="\033[1;32m"
+B_YELLOW="\033[1;33m"
+
 if [ "$(lsmod | grep ^xt_geoip)" ]; then
     curl -s "https://raw.githubusercontent.com/0xNeu/GFIGeoIP/main/Aggregated_Data/agg_cidrs.csv" >/tmp/agg_cidrs.csv
 
@@ -8,12 +16,12 @@ if [ "$(lsmod | grep ^xt_geoip)" ]; then
         echo -e "${B_GREEN}Already on the latest database! ${RESET}"
         rm /tmp/agg_cidrs.csv
     else
-        sudo mv /tmp/agg_cidrs.csv /usr/libexec/rainb0w/agg_cidrs.csv
+        mv /tmp/agg_cidrs.csv /usr/libexec/rainb0w/agg_cidrs.csv
         # Convert CSV database to binary format for xt_geoip
         echo -e "${B_GREEN}Newer aggregated CIDR database found, updating now... ${RESET}"
-        sudo /usr/libexec/rainb0w/xt_geoip_build_agg -s -i /usr/libexec/rainb0w/agg_cidrs.csv
+        /usr/libexec/rainb0w/xt_geoip_build_agg -s -i /usr/libexec/rainb0w/agg_cidrs.csv
         # Load xt_geoip kernel module
-        sudo modprobe xt_geoip
+        modprobe xt_geoip
         lsmod | grep ^xt_geoip
     fi
 fi
